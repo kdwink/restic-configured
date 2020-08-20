4aec6224d853ef6dba05e4e2d644692fce9a58df
Keith David Winkler <keithdwinkler@gmail.com>
2020-08-19 20:51:46 -0500

    improve

--- restic-backup.py
+++ restic-backup.py
@@ -11,11 +11,13 @@ if len(sys.argv) != 3:
 
 
 def print_config(config):
+    print("------------------------------------------------------")
     print(f"repository = {config['repository']}")
     for path in config['backup-paths']:
         print(f"path = {path['path']}")
     for exclude in config['exclude-patterns']:
         print(f"exclude = {exclude['pattern']}")
+    print("------------------------------------------------------")
 
 
 def read_config(file):
@@ -25,10 +27,13 @@ def read_config(file):
         return json.loads(json_string)
 
 
-def main(config_file_path):
+def main(config_file_path, command):
     print(f'configuration file path = {config_file_path}')
     config = read_config(config_file_path)
     print_config(config)
 
+    if command == 'init':
+        print("init")
 
-main(sys.argv[1])
+
+main(sys.argv[1], sys.argv[2])
