a69afc0189dd5f1471a3a8fb214fa72b1feead94
Keith David Winkler <keith.winkler@bayer.com>
2020-08-26 12:09:29 -0500

    kdwink-mac

--- restic-backup.py
+++ restic-backup.py
@@ -62,14 +62,18 @@ def banner(message):
 def format_command(command_part_array):
     result = ""
     length = 0
+    last_part = ""
     for part in command_part_array:
         new_len = length + len(part)
         new_line = (part.startswith("--") and (new_len > 40)) or (new_len > 80)
         if new_line:
             length = 0
             result = result + "\\\n\t"
-        result = result + (part if ' ' not in part else f'"{part}"') + " "
+        should_quote_part = (' ' in part or last_part == '--exclude')
+        result = result + (part if not should_quote_part else f'"{part}"')
+        result = result + " "
         length = length + len(part)
+        last_part = part
     return result
 
 # --------------------------------------------------------------------
