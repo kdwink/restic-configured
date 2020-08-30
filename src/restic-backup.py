66f764706f3542cc33f9a981e2686f956c77ca89
Keith David Winkler <keithdwinkler@gmail.com>
2020-08-29 22:34:51 -0500

    arg parser improvements

--- src/restic-backup.py
+++ src/restic-backup.py
@@ -12,17 +12,18 @@
 # INIT          : ./restic-backup.py config/example.json init
 # LIST SNAPSHOTS: ./restic-backup.py config/example.json snapshots
 # BACKUP        : ./restic-backup.py config/example.json backup
-# LIST FILES    : ./restic-backup.py config/example.json ls latest
+# LIST FILES    : ./restic-backup.py config/example.json ls <snapshot|latest>
 # CHECK         : ./restic-backup.py config/example.json check
 # STATS         : ./restic-backup.py config/example.json stats
 # PRUNE         : ./restic-backup.py config/example.json prune
-# RESTORE       : ./restic-backup.py config/example.json restore latest /etc /tmp/restored/etc
+# RESTORE       : ./restic-backup.py config/example.json restore <snapshot|latest> /etc /tmp/restored/etc
 # UNLOCK        : ./restic-backup.py config/example.json unlock
 #
 # DOCS:
 #
 # https://restic.readthedocs.io/en/latest/
 # ==============================================================================
+import argparse
 import datetime
 import os
 import subprocess
@@ -30,18 +31,11 @@ import sys
 import time
 import config as conf
 
-script_dir = os.path.dirname(os.path.realpath(__file__))
 
 # --------------------------------------------------------------------
-#
 # general purpose functions
 #
 # --------------------------------------------------------------------
-
-
-if len(sys.argv) < 3 or len(sys.argv) > 6:
-    print(f'usage: {sys.argv[0]} <config-file> <command>')
-    exit(-1)
 
 
 def redirect_stdout(config):
@@ -49,6 +43,7 @@ def redirect_stdout(config):
     log_dir = config.log_directory
     if not os.path.isdir(log_dir):
         os.makedirs(log_dir)
+    script_dir = os.path.dirname(os.path.realpath(__file__))
     log_file = f"{script_dir}/{log_dir}/{timestamp}.log"
     sys.stdout = open(log_file, 'w')
 
@@ -83,8 +78,8 @@ def format_command(command_part_array):
 #
 # --------------------------------------------------------------------
 
-def execute_restic(config, additional_args):
-    password_command = f"{sys.argv[0]} {sys.argv[1]} password"
+def execute_restic(config, args, additional_args):
+    password_command = f"{sys.argv[0]} {args.config_file} password"
     args = [
                "restic",
                "--repo", config.repository,
@@ -101,56 +96,59 @@ def execute_restic(config, additional_args):
     print(t.stdout)
 
 
-def command_check(config): execute_restic(config, ['check'])
+def command_check(config, args): execute_restic(config, args, ['check'])
 
 
-def command_stats(config):  execute_restic(config, ['stats', '--mode', 'raw-data'])
+def command_stats(config, args):  execute_restic(config, args, ['stats', '--mode', 'raw-data'])
 
 
-def command_prune(config):
-    execute_restic(config, ['prune'])
+def command_prune(config, args): execute_restic(config, args, ['prune'])
 
 
-def command_forget(config):
+def command_forget(config, args):
     for backup_path in config.backup_paths:
         policy = config.forget_policy if not backup_path.has_forgets() else backup_path.forget_policy
-        execute_restic(config, ['forget', '--path', backup_path.path] + policy)
+        execute_restic(config, args, ['forget', '--path', backup_path.path] + policy)
 
 
-def command_ls(config):
-    if len(sys.argv) != 4:
-        print(f"usage: {sys.argv[0]} [config-file] ls [snapshot|'latest']")
+def command_ls(config, args):
+    command = args.sub_command
+    if len(command) != 2:
+        print(f"usage: {command[0]} [snapshot|'latest']")
         exit(-1)
-    execute_restic(config, ['ls', '--long', sys.argv[3]])
+    execute_restic(config, args, ['ls', '--long', command[1]])
 
 
-def command_restore(config):
-    if len(sys.argv) != 6:
-        print(f"usage: {sys.argv[0]} [config-file] restore [snapshot|'latest'] [restore-path] [extract-to-path]")
+def command_restore(config, args):
+    command = args.sub_command
+    if len(command) != 4:
+        print(f"usage: {command[0]} [snapshot|'latest'] [restore-path] [extract-to-path]")
         exit(-1)
-    execute_restic(config, ['restore', sys.argv[3], '--path', sys.argv[4], '--target', sys.argv[5]])
+    execute_restic(config, args, ['restore', command[1], '--path', command[2], '--target', command[3]])
 
 
-def command_backup(config):
+def command_backup(config, args):
     for backup_path in config.backup_paths:
         banner(f"backing up '{backup_path.path}'")
         a = ["backup", "--one-file-system", backup_path.path]
         if backup_path.has_excludes():
             for e in backup_path.excludes:
                 a = a + ['--exclude', e.pattern]
-        execute_restic(config, a)
+        execute_restic(config, args, a)
 
 
-def command_snapshots(config):
-    execute_restic(config, ['snapshots'])
+def command_snapshots(config, args): execute_restic(config, args, ['snapshots'])
 
 
-def command_unlock(config):
-    execute_restic(config, ['unlock'])
+def command_unlock(config, args): execute_restic(config, args, ['unlock'])
 
 
-def command_init(config):
-    execute_restic(config, ['init'])
+def command_init(config, args): execute_restic(config, args, ['init'])
+
+
+def command_password(config, args):
+    print(config.password, end='')
+    exit(0)
 
 
 # --------------------------------------------------------------------
@@ -160,48 +158,51 @@ def command_init(config):
 # --------------------------------------------------------------------
 
 
-def main(config_file_path, command):
+def main():
     start_time = time.perf_counter()
-    config = conf.read_config(config_file_path)
 
-    if command == 'password':
-        print(config.password, end='')
-        exit(0)
+    parser = argparse.ArgumentParser(description='Restic backup tool.', add_help=True, allow_abbrev=False)
+    parser.add_argument("config_file", help="Configuration file.")
+    parser.add_argument("sub_command", help="Sub command.", nargs='*')
+    parser.add_argument('--log', action='store_true')
+    parser.add_argument('--version', action='version', version='%(prog)s 2.0')
+    args = parser.parse_args()
 
-    redirect_commands = ['backup', 'prune']
+    valid_commands = {
+        'backup': command_backup,
+        'check': command_check,
+        'forget': command_forget,
+        'init': command_init,
+        'ls': command_ls,
+        'password': command_password,
+        'prune': command_prune,
+        'restore': command_restore,
+        'stats': command_stats,
+        'snapshots': command_snapshots,
+        'unlock': command_unlock,
+    }
 
-    if command in redirect_commands:
+    if len(args.sub_command) < 1:
+        print(f"missing sub-command: {list(valid_commands.keys())}")
+        sys.exit(-1)
+
+    if args.sub_command[0] not in valid_commands:
+        print(f"BAD sub-command: '{args.sub_command[0]}'")
+        sys.exit(-1)
+
+    config = conf.read_config(args.config_file)
+
+    if args.log:
         redirect_stdout(config)
 
-    conf.print_config(config)
+    if args.sub_command[0] != 'password':
+        banner("config")
+        conf.print_config(config)
+        banner("starting")
 
-    banner("starting")
-
-    if command == 'init':
-        command_init(config)
-    elif command == 'unlock':
-        command_unlock(config)
-    elif command == 'forget':
-        command_forget(config)
-    elif command == 'snapshots':
-        command_snapshots(config)
-    elif command == 'check':
-        command_check(config)
-    elif command == 'stats':
-        command_stats(config)
-    elif command == 'ls':
-        command_ls(config)
-    elif command == 'restore':
-        command_restore(config)
-    elif command == 'backup':
-        command_backup(config)
-    elif command == 'prune':
-        command_prune(config)
-    else:
-        print(f"BAD COMMAND: {command}")
-        exit(0)
+    valid_commands[args.sub_command[0]](config, args)
 
     banner(f"COMPLETE in {time.perf_counter() - start_time:,.0f} seconds.")
 
 
-main(sys.argv[1], sys.argv[2])
+main()
