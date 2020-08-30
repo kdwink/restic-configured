933d52526df88f57adae57d76651f1379b7a108d
Keith David Winkler <keithdwinkler@gmail.com>
2020-08-30 17:51:28 -0500

    logging

--- src/restic/logging.py
+++ src/restic/logging.py
@@ -1,9 +1,25 @@
 import datetime
 import os
 import sys
+import time
 
 
 def timestamp(): return datetime.datetime.now().replace(microsecond=0).isoformat('_')
+
+
+def _file_age_seconds(filepath):
+    return time.time() - os.path.getmtime(filepath)
+
+
+def _delete_old_logs(log_dir):
+    max_age_seconds = 60 * 60 * 24 * 14
+    for root, dirs, files in os.walk(log_dir):
+        for file in files:
+            log_file = os.path.join(root, file)
+            age_seconds = _file_age_seconds(log_file)
+            if age_seconds > max_age_seconds:
+                os.remove(log_file)
+                banner(f"deleting old log file: age={age_seconds}; log_file={log_file};")
 
 
 def redirect_stdout(config, relative_dir):
@@ -12,6 +28,7 @@ def redirect_stdout(config, relative_dir):
     if not os.path.isdir(d):
         os.makedirs(d)
     sys.stdout = open(f"{d}/{timestamp()}.log", 'w')
+    _delete_old_logs(d)
 
 
 def banner(message):
