40dc0ea9fdb7d6b7623a1740560538a3dcc37617
Keith David Winkler <keithdwinkler@gmail.com>
2020-09-02 22:25:11 -0500

    VERSION: 7

--- package.sh
+++ package.sh
@@ -1,3 +1,18 @@
 # exit script on any error
 trap 'exit' ERR
 
+#----------------------------------------------------------------
+# increment version
+#----------------------------------------------------------------
+
+VERSION_FILE="src/restic/version.txt"
+HASH=`git rev-parse --short HEAD`
+
+oldnum=`grep version ${VERSION_FILE} | cut -d "=" -f2`
+newnum=`expr $oldnum + 1`
+sed -i "s/version=$oldnum\$/version=$newnum/g" ${VERSION_FILE}
+sed -i "s/hash=.*\$/hash=$HASH/g" ${VERSION_FILE}
+
+
+git commit -m "VERSION: $newnum" -a
+git push origin HEAD
