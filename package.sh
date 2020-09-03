582db26f48f530dc3be053e22928b3f2bd8db9b8
Keith David Winkler <keithdwinkler@gmail.com>
2020-09-02 22:28:40 -0500

    VERSION: 2

--- package.sh
+++ package.sh
@@ -9,7 +9,7 @@ VERSION_FILE="src/restic/version.txt"
 HASH=`git rev-parse --short HEAD`
 
 OLD_VER=`grep version ${VERSION_FILE} | cut -d "=" -f2`
-NEW_VER=`expr $oldnum + 1`
+NEW_VER=`expr ${OLD_VER} + 1`
 sed -i "s/version=${OLD_VER}\$/version=${NEW_VER}/g" ${VERSION_FILE}
 sed -i "s/hash=.*\$/hash=$HASH/g" ${VERSION_FILE}
 
@@ -22,4 +22,4 @@ git push origin HEAD
 # package src
 #----------------------------------------------------------------
 
-tar --create --verbose --gzip --file restic-backup-v${NEW_VER}.tgz
+tar --create --verbose --gzip --file restic-backup-v${NEW_VER}.tgz src
