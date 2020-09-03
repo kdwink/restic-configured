1e30d15f91d186a54708001e9fff0b7c0edae0e4
Keith David Winkler <keithdwinkler@gmail.com>
2020-09-02 22:27:48 -0500

    VERSION: 1

--- package.sh
+++ package.sh
@@ -8,11 +8,18 @@ trap 'exit' ERR
 VERSION_FILE="src/restic/version.txt"
 HASH=`git rev-parse --short HEAD`
 
-oldnum=`grep version ${VERSION_FILE} | cut -d "=" -f2`
-newnum=`expr $oldnum + 1`
-sed -i "s/version=$oldnum\$/version=$newnum/g" ${VERSION_FILE}
+OLD_VER=`grep version ${VERSION_FILE} | cut -d "=" -f2`
+NEW_VER=`expr $oldnum + 1`
+sed -i "s/version=${OLD_VER}\$/version=${NEW_VER}/g" ${VERSION_FILE}
 sed -i "s/hash=.*\$/hash=$HASH/g" ${VERSION_FILE}
 
 
-git commit -m "VERSION: $newnum" -a
+git commit -m "VERSION: ${NEW_VER}" -a
 git push origin HEAD
+
+
+#----------------------------------------------------------------
+# package src
+#----------------------------------------------------------------
+
+tar --create --verbose --gzip --file restic-backup-v${NEW_VER}.tgz
